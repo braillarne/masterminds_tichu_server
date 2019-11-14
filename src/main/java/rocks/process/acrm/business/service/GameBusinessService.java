9506a51b7dad7cf9502e1b2e20cfc1ec2720de73
Soltox <pascal.schaller@outlook.com>
2019-11-14 16:30:56 +0100

    Created isCombination methods without being fitted to api architecture yet

--- src/main/java/rocks/process/acrm/business/service/GameBusinessService.java
+++ src/main/java/rocks/process/acrm/business/service/GameBusinessService.java
@@ -5,6 +5,7 @@ import rocks.process.acrm.data.domain.*;
 import rocks.process.acrm.data.repository.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class GameBusinessService {
@@ -23,11 +24,110 @@ public class GameBusinessService {
     public void evaluatePlayableCombinationsFromPlayerHand(List<Card> hand) {
     }
 
+    public boolean isPair(List<Card> transmittedcards) {
+        if (transmittedcards.size() == 2 && transmittedcards.get(0).getRank() == transmittedcards.get(1).getRank()) {
+            return true;
+
+        } else return false;
+    }
+
+    public boolean isTriple(List<Card> transmittedcards) {
+        if (transmittedcards.size() == 3
+                && transmittedcards.get(0).getRank() == transmittedcards.get(1).getRank()
+                && transmittedcards.get(0).getRank() == transmittedcards.get(2).getRank()) {
+            return true;
+
+        } else return false;
+    }
+
+    public boolean isRunningPair(List<Card> transmittedCards) {
+        transmittedCards.sort(Comparator.comparing(Card::getRank));
+        if (transmittedCards.size() < 4) return false;
+        int counter = 0;
+        for (int i = 1; i < transmittedCards.size() - 1; i = i + 2) {
+            if (transmittedCards.get(i).getRank() + 1 == transmittedCards.get(i + 1).getRank()) {
+                counter++;
+            }
+        }
+        //n pairs take n-1 comparisons. We determine n pairs by dividing with two.
+        if (counter == transmittedCards.size() / 2 - 1) return true;
+        return false;
+    }
+
+    public boolean isFullHouse(List<Card> transmittedCards) {
+        if (transmittedCards.size() != 5) return false;
+        transmittedCards.sort(Comparator.comparing(Card::getRank));
+
+        //Check xxxyy
+        if (transmittedCards.get(0).getRank() == transmittedCards.get(1).getRank()
+                && transmittedCards.get(1).getRank() == transmittedCards.get(2).getRank()
+                && transmittedCards.get(3).getRank() == transmittedCards.get(4).getRank()) {
+
+            return true;
+        }
+
+
+        //Check xxyyy
+        if (transmittedCards.get(0).getRank() == transmittedCards.get(1).getRank()
+                && transmittedCards.get(2).getRank() == transmittedCards.get(3).getRank()
+                && transmittedCards.get(3).getRank() == transmittedCards.get(4).getRank()) {
+
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public boolean isRow(List<Card> transmittedCards) {
+        int counter = 0;
+        if (transmittedCards.size() < 5) return false;
+
+        for (int i = 0; i < transmittedCards.size() - 1; i++) {
+            if (transmittedCards.get(i).getRank() + 1 == transmittedCards.get(i + 1).getRank()) {
+                counter++;
+
+            }
+
+        }
+        //n sequence elements need n-1 comparisons
+        if (counter+1 == transmittedCards.size()) return true;
+
+        return false;
+    }
+
+    public boolean isBomb(List<Card> transmittedCards) {
+        int counter = 0;
+
+        if(transmittedCards.size()==4){
+            for(int i = 0; i<transmittedCards.size()-1;i++){
+                if (transmittedCards.get(i).getRank() == transmittedCards.get(i + 1).getRank()) {
+                    counter++;
+                }
+            }
+            //n elements of a sequence need n-1 comparisons
+            if (counter+1 == transmittedCards.size()) return true;
+        }
+
+        if(transmittedCards.size()>=5){
+            for(int i = 0; i<transmittedCards.size()-1;i++) {
+            if(transmittedCards.get(i).getRank()+1==transmittedCards.get(i+1).getRank()
+            &&transmittedCards.get(i).getSuit().equals(transmittedCards.get(i+1).getSuit())){
+                counter++;
+            }
+            }
+            if(counter+1 == transmittedCards.size()) return true;
+            }
+
+return false;
+    }
+
+
     public List<Combination> addPossiblePairsToPlayableCombinations(List<Card> hand) {
         List<Combination> combinationList = new ArrayList<>();
 
         for (int i = 0; i < hand.size(); i++) {
-            for (int j = i+1; j < hand.size(); j++) {
+            for (int j = i + 1; j < hand.size(); j++) {
                 if (hand.get(i).getRank() == hand.get(j).getRank()) {
                     List<Card> temp = new ArrayList<>();
                     temp.add(hand.get(i));
@@ -46,17 +146,17 @@ public class GameBusinessService {
         if (pairs.size() < 2) {
             return runningPairs;
         } else {
-            for (int i = 0; i < pairs.size()-1; i++) {
+            for (int i = 0; i < pairs.size() - 1; i++) {
                 List<Card> temp = new ArrayList<>();
                 if (pairs.get(i).getMainRank() + 1 == pairs.get(i + 1).getMainRank()) {
                     if (!temp.contains(pairs.get(i).getCards())) {
-                        for (Card c:pairs.get(i).getCards()
-                             ) {
+                        for (Card c : pairs.get(i).getCards()
+                        ) {
                             temp.add(c);
                         }
                     }
                     if (!temp.contains(pairs.get(i + 1).getCards())) {
-                        for (Card c:pairs.get(i+1).getCards()
+                        for (Card c : pairs.get(i + 1).getCards()
                         ) {
                             temp.add(c);
                         }
@@ -64,8 +164,8 @@ public class GameBusinessService {
 
                 }
                 int score = 0;
-                for (Card c:temp
-                     ) {
+                for (Card c : temp
+                ) {
                     score += c.getRank();
                 }
                 runningPairs.add(new Combination(temp, CombinationType.RUNPAIR, score));
