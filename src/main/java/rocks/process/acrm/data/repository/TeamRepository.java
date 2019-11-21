d2270ef94617d62e98271f5da5561e74252babe3
Soltox <pascal.schaller@outlook.com>
2019-11-21 17:08:48 +0100

    Created TeamRepository

---
+++ src/main/java/rocks/process/acrm/data/repository/TeamRepository.java
@@ -0,0 +1,17 @@
+package rocks.process.acrm.data.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rocks.process.acrm.data.domain.Team;
+
+
+
+    @Repository
+    public interface TeamRepository extends JpaRepository<Team, Long>{
+
+
+
+    }
+
+
+
